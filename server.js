b63aeba82cb35a8f98794ba7b55420755bf7e220
JEET AHIRWAR <jeetahirwar664@gmail.com>
2026-05-03 12:04:51 +0530

    port

--- server.js
+++ server.js
@@ -16,4 +16,9 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+{
+    console.log(`Server running on port ${PORT}`);
+});
